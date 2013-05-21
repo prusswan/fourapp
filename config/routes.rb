7d3c9ec08666874b8e8ac4a0b540052dae06ffca
prusswan <prusswan@gmail.com>
2013-05-22 02:24:57 +0800

    chapter 2: gfx, game

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Fourapp::Application.routes.draw do
-  get "pages/index"
+  get "pages/rumble"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
